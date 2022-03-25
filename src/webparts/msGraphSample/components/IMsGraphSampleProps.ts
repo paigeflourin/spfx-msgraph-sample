ee0b42d7c659bc4a4b29e59037d94ea5d0e1672e
ivan posadas <ivan.posadas@moveassist.com>
2022-03-25 17:52:01 +0800

    initial commit for spfx scaffolding

---
+++ src/webparts/msGraphSample/components/IMsGraphSampleProps.ts
@@ -0,0 +1,7 @@
+export interface IMsGraphSampleProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/msGraphSample/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMsGraphSampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MsGraphSampleWebPartStrings' {
+  const strings: IMsGraphSampleWebPartStrings;
+  export = strings;
+}
